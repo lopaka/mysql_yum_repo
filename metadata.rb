6c1ea9fb7ba7696a0be9fba679e881cdb430dd0b
Lopaka Delp <lopaka@rightscale.com>
2015-06-15 21:33:28 -0700

    Create default recipe.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
 name             'mysql_yum_repo'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Lopaka Delp'
+maintainer_email 'lopaka@rightscale.com'
+license          'Apache 2.0'
 description      'Installs/Configures mysql_yum_repo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'chef-sugar'
+
+recipe 'mysql_yum_repo::default', 'Install mysql 5.5 from mysql repo'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,24 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'chef-sugar::default'
+
+compile_time do
+  cookbook_file 'mysql-community-release-el7-5.noarch.rpm' do
+    path '/tmp/mysql-community-release-el7-5.noarch.rpm'
+    action :create_if_missing
+  end
+
+  cookbook_file 'mysql-community.repo' do
+    path '/etc/yum.repos.d/mysql-community.repo'
+    action :create_if_missing
+  end
+
+  rpm_package 'mysql repository' do
+    source '/tmp/mysql-community-release-el7-5.noarch.rpm'
+    action :install
+  end
+
+  yum_package 'mysql-community-server'
+end
